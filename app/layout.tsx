16dd8106c7a52edc0b6742995d7163238fd0445a
Samanvith <samanvithmn135@gmail.com>
2026-07-14 22:17:44 +0530

    Fix navigation loader build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   <LoadingProvider>
 
   <NavigationEvents />
-
+  
   <ImageViewerProvider>
 
     <LoadingScreen />
--- src/components/NavigationEvents.tsx
+++ src/components/NavigationEvents.tsx
@@ -1,25 +1,22 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useLoading } from '@/context/LoadingContext';
 
 export default function NavigationEvents() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-
   const { startLoading, stopLoading } = useLoading();
 
-  // Show loader immediately when component mounts
   useEffect(() => {
     startLoading();
 
     const timer = setTimeout(() => {
       stopLoading();
-    }, 500); // Minimum visible duration
+    }, 500);
 
     return () => clearTimeout(timer);
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
